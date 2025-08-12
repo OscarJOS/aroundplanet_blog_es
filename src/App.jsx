5c794ac47cd78bbb6cb2c26d998786ef47935e46
Oscar Forero <oscar.forero@talently.tech>
2025-08-12 03:33:06 -0500

    Fix paths

--- src/App.jsx
+++ src/App.jsx
@@ -16,19 +16,19 @@ function App() {
   const [regionData, setRegionData] = useState([])
 
   useEffect(() => {
-    fetch('/data/avg_total.json')
+    fetch(`${process.env.PUBLIC_URL}/data/avg_total.json`)
       .then(res => res.json())
       .then(data => setAvgData(data))
-    
-    fetch('/data/top3.json')
+
+    fetch(`${process.env.PUBLIC_URL}/data/top3.json`)
       .then(res => res.json())
       .then(data => setTop3Data(data.data))
-    
-    fetch('/data/last3.json')
+
+    fetch(`${process.env.PUBLIC_URL}/data/last3.json`)
       .then(res => res.json())
       .then(data => setLast3Data(data.data))
-    
-    fetch('/data/region.json')
+
+    fetch(`${process.env.PUBLIC_URL}/data/region.json`)
       .then(res => res.json())
       .then(data => setRegionData(data.data))
   }, [])
